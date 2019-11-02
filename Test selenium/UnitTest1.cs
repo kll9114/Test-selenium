e8f282de2cc2f51753d69e7e6da0a0b3c9e60ef8
Mohammed Amin KOULAL <mohammedkoulal@yahoo.fr>
2019-11-02 22:08:42 +0100

    test

--- Test selenium/UnitTest1.cs
+++ Test selenium/UnitTest1.cs
@@ -1,18 +1,25 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+ 
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
-        }
+        IWebDriver driver = new ChromeDriver(@"C:\Users\Utilisateur\Documents\Visual Studio 2015\Projects\Onlystore\packages\WebDriver.ChromeDriver.win32.76.0.3809.68\content");
+        driver.Url = ("http://toolsqa.com/Automation-practice-form/");
+
+
+        // Type Name in the FirstName text box      
+        driver.FindElement(By.Name("firstname")).SendKeys("Lakshay");
+
+           
+                }
     }
 }
